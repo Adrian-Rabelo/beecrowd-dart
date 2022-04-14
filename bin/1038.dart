00c75bfceb7665bcdee7666c5afeed1d51f19e36
Adrian Rabelo <adrianrabeloe10@gmail.com>
2022-04-13 21:36:04 -0400

    1038

--- bin/1038.dart
+++ bin/1038.dart
@@ -1,26 +1,28 @@
 import 'dart:io';
 
 void main(List<String> args) {
-  String resultado = stdin.readLineSync();
-  int codigo = int.parse(resultado[0]);
-  int qnt = int.parse(resultado[2]);
-  double total = 0;
-  switch (codigo) {
-    case 1:
-      total = qnt * 4.00;
-      break;
-    case 2:
-      total = qnt * 4.50;
-      break;
-    case 3:
-      total = qnt * 5.00;
-      break;
-    case 4:
-      total = qnt * 2.00;
-      break;
-    case 5:
-      total = qnt * 1.50;
-      break;
+  var resultado = stdin.readLineSync();
+  if (resultado != null) {
+    int codigo = int.parse(resultado[0]);
+    int qnt = int.parse(resultado[2]);
+    double total = 0;
+    switch (codigo) {
+      case 1:
+        total = qnt * 4.00;
+        break;
+      case 2:
+        total = qnt * 4.50;
+        break;
+      case 3:
+        total = qnt * 5.00;
+        break;
+      case 4:
+        total = qnt * 2.00;
+        break;
+      case 5:
+        total = qnt * 1.50;
+        break;
+    }
+    print('Total: R\$ ${total.toStringAsFixed(2)}');
   }
-  print('Total: R\$ ${total.toStringAsFixed(2)}');
 }
