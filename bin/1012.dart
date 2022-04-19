0536a99841e83fe5fbd62627493b80c5bbf4ec90
adrian.rabelo <adrian.mendes@tjro.jus.br>
2022-04-19 12:50:20 -0400

    1012

--- bin/1012.dart
+++ bin/1012.dart
@@ -1,17 +1,5 @@
 import 'dart:io';
 
-/* a) a área do triângulo retângulo que tem A por base e C por altura.
- * b) a área do círculo de raio C. (pi = 3.14159)
- * c) a área do trapézio que tem A e B por bases e C por altura.
- * d) a área do quadrado que tem lado B.
- * e) a área do retângulo que tem lados A e B.
- * 3.0 4.0 5.2
- * TRIANGULO: 7.800
- * CIRCULO: 84.949
- * TRAPEZIO: 18.200
- * QUADRADO: 16.000
- * RETANGULO: 12.000
-*/
 void main() {
   var valores = stdin.readLineSync() ?? '';
 
@@ -23,5 +11,5 @@ void main() {
   print('CIRCULO: ${(3.14159 * (c * c)).toStringAsFixed(3)}');
   print('TRAPEZIO: ${(((a + b) * c) / 2).toStringAsFixed(3)}');
   print('QUADRADO: ${(b * b).toStringAsFixed(3)}');
-  print('TRIANGULO: ${(a * b).toStringAsFixed(3)}');
+  print('RETANGULO: ${(a * b).toStringAsFixed(3)}');
 }
